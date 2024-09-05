cc038e08e32e442a39d1af4acd1bc7b2ee5062a5
keithmmc <100876230+keithmmc@users.noreply.github.com>
2024-09-05 22:47:40 +0100

    Update chohan.py

--- chohan.py
+++ chohan.py
@@ -17,4 +17,43 @@ while True:
         else:
             pot = int(pot)
             break 
-    
+        
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+print('The dealer swirls the cup and you hear the rattle of dice.')
+print('The dealer slams the cup on the floor, still covering the')
+print('dice and asks for your bet.')
+print()
+print('    CHO (even) or HAN (odd)?')  
+
+
+while True:
+    bet = input('>').upper()
+    if bet != 'CHO' and bet != 'HAN':
+        print('Please enter either "CHO" or "HAN".')
+        continue
+    else:
+        break 
+    print('The dealer lifts the cup to reveal:')
+print('  ', JAPANESE_NUMBERS[dice1], '-', JAPANESE_NUMBERS[dice2])
+print('    ', dice1, '-', dice2)
+rollIsEven = (dice1 + dice2) % 2 == 0
+
+if rollIsEven:
+        correctBet = 'CHO'
+else:
+    correctBet = 'HAN'
+playerWon = bet == correctBet
+
+if playerWon:
+    print('You won! You take', pot, 'mon.')
+    purse = purse + pot  
+    print('The house collects a', pot // 10, 'mon fee.')
+    purse = purse - (pot // 10)  
+else:
+  purse = purse - pot  
+print('You lost!')
+if purse == 0:
+    print('You have run out of money!')
+    print('Thanks for playing!')
+    sys.exit()
