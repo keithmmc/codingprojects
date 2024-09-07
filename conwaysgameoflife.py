8a3395076355e25ff97963dd94ea894f80446722
keithmmc <100876230+keithmmc@users.noreply.github.com>
2024-09-08 00:22:22 +0100

    Update conwaysgameoflife.py

--- conwaysgameoflife.py
+++ conwaysgameoflife.py
@@ -11,4 +11,30 @@ for x in range(WIDTH):
             nextCells[(x, y)] = ALIVE
         else:
             nextCells[(x, y)] = DEAD 
-            
+            while True:
+                print('\n' * 50)
+                cells = copy.deepcopy(nextCells)
+                for y in range(HEIGHT):
+                    for x in range(WIDTH):
+                        print(cells[(x, y)], end='')
+                    print()
+                print('Press Ctrl-C to quit')
+                for x in range(WIDTH):
+                    for y in range(HEIGHT):
+                        left = (x - 1) % WIDTH
+                        right = (x + 1) % WIDTH
+                        above = (y - 1) % HEIGHT 
+                        below = (y + 1) % HEIGHT
+                        numNeighbors = 0
+                if cells[(x, y )] == ALIVE and (numNeighbors == 2 or numNeighbors == 3):
+                    nextCells[(x, y)] = ALIVE
+                elif cells[(x, y)] == DEAD and numNeighbors == 3:
+                    nextCells[(x, y)] = ALIVE
+                else:
+                    nextCells[(x, y)] = DEAD 
+            try:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                print("conways game of life")
+                ses.exit()
+                
